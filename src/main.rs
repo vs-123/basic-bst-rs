fdde67f8c7216eb844885f9b44583236075b3864
vs-123 <19247154+vs-123@users.noreply.github.com>
2022-01-15 17:59:04 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::cmp::Ordering;
 
 #[derive(Debug, Clone)]
-struct BinTreeNode<T>
-where
-    T: Ord + Clone,
-{
+struct BinTreeNode<T> {
     value: T,
     left: Option<Box<BinTreeNode<T>>>,
     right: Option<Box<BinTreeNode<T>>>,
@@ -12,78 +9,55 @@ where
 
 impl<T> BinTreeNode<T>
 where
-    T: Ord + Clone,
+    T: Ord,
 {
-    fn from_vec(vec: Vec<T>) -> Option<Self> {
-        if vec.is_empty() {
-            return None;
-        }
+    fn from_vec(vec: Vec<T>) -> Result<Self, &'static str> {
+        let mut items = vec.into_iter();
+
+        let first = items.next().ok_or("Empty vector")?;
 
         let mut output = BinTreeNode {
-            value: vec[0].clone(),
+            value: first,
             left: None,
             right: None,
         };
 
-        if vec.len() == 1 {
-            return Some(output);
+        for item in items {
+            output.insert(item);
         }
 
-        for i in vec.iter().skip(1) {
-            output.insert(i.clone());
-        }
-
-        Some(output)
+        Ok(output)
     }
 
     fn insert(&mut self, value: T) {
-        let mut current_node = Box::new(self.clone());
-        loop {
-            match value.cmp(&current_node.value) {
-                Ordering::Greater => {
-                    if current_node.clone().right.is_some() {
-                        current_node.as_mut().right.as_mut().unwrap().insert(value);
-                        break;
-                    }
+        let mut current = self;
 
-                    current_node.right = Some(Box::new(BinTreeNode {
-                        value,
-                        left: None,
-                        right: None,
-                    }));
-                    break;
-                }
+        let insert_node = loop {
+            let next_node = match value.cmp(&current.value) {
+                Ordering::Less | Ordering::Equal => &mut current.left,
+                Ordering::Greater => &mut current.right,
+            };
 
-                Ordering::Less => {
-                    if current_node.clone().left.is_some() {
-                        current_node.as_mut().left.as_mut().unwrap().insert(value);
-                        break;
-                    }
-
-                    current_node.left = Some(Box::new(BinTreeNode {
-                        value,
-                        left: None,
-                        right: None,
-                    }));
-                    break;
-                }
-
-                Ordering::Equal => continue,
+            match next_node {
+                Some(ref mut node) => current = node,
+                None => break next_node,
             }
-        }
-        *self = *current_node;
+        };
+
+        *insert_node = Some(Box::new(BinTreeNode {
+            value,
+            left: None,
+            right: None,
+        }));
     }
 
-    fn contains(&self, value: T) -> bool {
-        let mut current_node = Some(Box::new(self.clone()));
-        loop {
-            if current_node.is_none() {
-                break;
-            }
-            let unwrapped_node = current_node.clone().unwrap();
-            match value.cmp(&unwrapped_node.value) {
-                Ordering::Greater => current_node = unwrapped_node.right,
-                Ordering::Less => current_node = unwrapped_node.left,
+    fn contains(&self, value: &T) -> bool {
+        let mut current = Some(self);
+
+        while let Some(node) = current {
+            match value.cmp(&node.value) {
+                Ordering::Greater => current = node.right.as_deref(),
+                Ordering::Less => current = node.left.as_deref(),
                 Ordering::Equal => return true,
             }
         }
@@ -93,6 +67,6 @@ where
 }
 
 fn main() {
-    let my_bin_tree = BinTreeNode::from_vec(vec![3, 5, 6, 1, 7, 8]).expect("Empty vector");
-    println!("{:?}", my_bin_tree.contains(3));
+    let my_bin_tree = BinTreeNode::from_vec(vec![3, 5, 6, 1, 7, 8]).unwrap();
+    println!("{:?}", my_bin_tree.contains(&3));
 }
